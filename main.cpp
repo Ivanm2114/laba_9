c203b67b65cbb96a89b45e29b9cba92c3f9e83bf
Ivan <67190437+Ivanm2114@users.noreply.github.com>
2023-04-10 10:14:30 +0300

    all works correctly

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 
 #define lmax 200
 
@@ -20,6 +21,7 @@ int find_substrings(string input[], string out[], int origins[], int len_input) 
     bool flag;
 
     for (int i = 0; i < len_input; i++) {
+        first=-1;
         for (int j = 0; j < input[i].length(); j++) {
             flag = is_needed_symbol(input[i][j]);
             if (flag && first < 0) {
@@ -47,23 +49,55 @@ int find_shortest(string input[], int len_input) {
     return mini;
 }
 
-bool insert_end(string& input) {
+bool insert_end(string &input) {
     bool change = false;
     int i = input.length() - 1;
-    while (i > 0 && input[i] != '*') {
+    while (i >= 0 && input[i] != '*') {
         --i;
     }
     if (i >= 0) {
         input.insert(i, "END");
-        change=true;
+        change = true;
     }
     return change;
 
 }
 
+
 int main() {
-    string abc="asda434*sds";
-    insert_end(abc);
-    cout << abc;
+    string input[lmax], good_strs[lmax],s;
+    int index_of_shortest_str, len_input, len_good_strs=0, indexes_of_origins[lmax];
+    bool added_END = false;
+    do {
+        cout << "Enter number of lines: ";
+        cin >> len_input;
+        if (cin.fail()) cin.clear();
+        getline(cin,s);
+    } while (len_input <= 0);
+    cout << "Enter " << len_input << " strings\n";
+    for(int i=0;i<len_input;++i){
+        getline(cin,input[i]);
+    }
+    cout << "-----------------------------------\nYour strings:\n";
+    for(int i=0;i<len_input;i++) cout<<input[i]+"\n";
+    len_good_strs= find_substrings(input,good_strs,indexes_of_origins,len_input);
+    cout << "-----------------------------------\nSuitable strings:\n";
+    if(len_good_strs){
+        for(int i=0;i<len_good_strs;i++) cout<<good_strs[i]+"\n";
+        index_of_shortest_str= find_shortest(good_strs,len_good_strs);
+        cout<<"-----------------------------------\nShortest substring:\n" << good_strs[index_of_shortest_str];
+        added_END=insert_end(input[indexes_of_origins[index_of_shortest_str]]);
+        if(added_END){
+            cout<<"\n-----------------------------------\nEdited original string:\n" <<input[indexes_of_origins[index_of_shortest_str]];
+        }
+        else{
+            cout<<"-----------------------------------\nThere is no * in string";
+        }
+    }
+    else{
+        cout <<"-----------------------------------\nNo suitable strings, can`t continue";
+    }
+
+
     return 0;
 }
